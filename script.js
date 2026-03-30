81cc105d13b6af481a01acc11790ee735b0c9aec
diionii <Ddion230@gmail.com>
2026-03-30 16:04:34 +0200

    App-7:Removed the search functionality
    Removed the search functionality from all the sections

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const searchForm = document.querySelector('.search-form');
-const searchBtn = document.querySelector('#search-btn');
+
 const cartItem = document.querySelector('.cart-items-container')
 const cartBtn = document.querySelector('#cart-btn')
 const navbar = document.querySelector('.navbar')
@@ -7,14 +6,7 @@ const menuBtn = document.querySelector('#menu-btn')
 
 
 
-searchBtn.addEventListener('click', ()=>{
-    searchForm.classList.toggle('active');
-    document.addEventListener('click', (e)=>{
-        if(!e.composedPath().includes(searchBtn) && !e.composedPath().includes(searchForm) ){
-            searchForm.classList.remove('active');
-        }
-    })
-})
+
 
 cartBtn.addEventListener('click', ()=>{
     cartItem.classList.toggle('active');
